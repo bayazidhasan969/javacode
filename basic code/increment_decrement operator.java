d70243ba2cba2f5fc07d2cb2c555f38bd379cf04
bayazidhasan969 <60389731+bayazidhasan969@users.noreply.github.com>
2020-01-29 23:21:15 +0600

    Create increment_decrement operator.java

---
+++ basic code/increment_decrement operator.java
@@ -0,0 +1,12 @@
+public class Increment {
+    public static void main(String[] args) {
+        int x=25;
+        int y;
+        y=x++;
+        System.out.println(y);
+        y=--x;
+        System.out.println(y);
+        y=++x;
+        System.out.println(y);
+    }
+    }
