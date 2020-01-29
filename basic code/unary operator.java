672fbad803e4997edcf17836556ed59dfd0842de
bayazidhasan969 <60389731+bayazidhasan969@users.noreply.github.com>
2020-01-29 23:23:03 +0600

    Create unary operator.java

---
+++ basic code/unary operator.java
@@ -0,0 +1,11 @@
+public class Unary {
+    public static void main(String[] args) {
+        int x=10;
+        int result;
+        result=+x;
+        System.out.println("ans = "+result);
+        result=-x;
+        System.out.println("ans = "+result);
+    }
+ 
+}
