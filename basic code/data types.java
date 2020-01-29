6271f8471e61808d5698d9dcd2e918a16976821b
bayazidhasan969 <60389731+bayazidhasan969@users.noreply.github.com>
2020-01-29 23:11:50 +0600

    Create data types.java

---
+++ basic code/data types.java
@@ -0,0 +1,21 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        boolean b;
+        int c;
+        char a;
+        double d;
+        String name;
+        b=true;
+        c=50;
+        d=2.30;
+        a='F';
+        name="bayazid hasan";
+        System.out.printf("this is boolean : %b\n",b);
+        System.out.printf("integer is = %d\n",c);
+        System.out.printf("float number is = %f\n",d);
+        System.out.printf("the character = %c\n",a);
+        System.out.printf("your name is :%s\n",name);
+       
+    }
+    
+}
