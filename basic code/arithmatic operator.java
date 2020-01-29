f95f8c83447780fd0927be51637f4ed1467c9fed
bayazidhasan969 <60389731+bayazidhasan969@users.noreply.github.com>
2020-01-29 23:17:53 +0600

    Create arithmatic operator.java

---
+++ basic code/arithmatic operator.java
@@ -0,0 +1,16 @@
+public class Arithmatic {
+    public static void main(String[] args) {
+        int num1,num2,result1,result2,result3,result4;
+        num1=20;
+        num2=10;
+        result1=num1+num2;
+        result2=num1-num2;
+        result3=num1*num2;
+        result4=num1/num2;
+        System.out.println("sum is "+result1);
+        System.out.println("substraction is "+result2);
+        System.out.println("multiplication is "+result3);
+        System.out.println("quatient is "+result4);
+    }
+    
+}
