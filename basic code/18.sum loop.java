44e20266179db80b259f36a04deae279bc6957a1
bayazidhasan969 <60389731+bayazidhasan969@users.noreply.github.com>
2020-01-30 17:33:38 +0600

    Create 18.sum loop.java

---
+++ basic code/18.sum loop.java
@@ -0,0 +1,10 @@
+public class SumLoop {
+    public static void main(String[] args) {
+        int sum=0,j;
+        for (j = 0; j <=10; j++) {
+         sum=sum+j;  
+           
+        }
+         System.out.println("sum = "+sum);
+    }
+}
