e0ac17b87e29e6968c98f43f0f64a539c992febb
bayazidhasan969 <60389731+bayazidhasan969@users.noreply.github.com>
2020-01-30 16:55:24 +0600

    Create 14.Math.java

---
+++ basic code/14.Math.java
@@ -0,0 +1,10 @@
+public class MathDemo {
+    public static void main(String[] args) {
+          int x=10;
+        int y=20;
+        int z=Math.min(x,y);
+        int a=Math.max(x,y);
+        System.out.println("minimum number :"+z);
+        System.out.println("maximum number :"+a);
+    }
+}
