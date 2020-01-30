13811f939edbb5829209b084f230405f0a69d16a
bayazidhasan969 <60389731+bayazidhasan969@users.noreply.github.com>
2020-01-30 17:22:51 +0600

    Create 17.break continue.java

---
+++ basic code/17.break continue.java
@@ -0,0 +1,19 @@
+public class Break{
+    public static void main(String[] args) {
+        //break statement
+        int i,j;
+        for(i=1;i<=100;i++){
+            if(i==10)
+                break;
+            
+        System.out.println(i);
+    }
+    //continue statement
+    for(j=1;j<=11;j++){
+    if(j==5)
+        continue;
+    System.out.println(j);
+    }
+}
+  
+}
