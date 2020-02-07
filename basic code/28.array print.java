a64e05141ce219ef0d25a55b7cd063a59f56c5fc
bayazidhasan969 <60389731+bayazidhasan969@users.noreply.github.com>
2020-02-07 22:14:11 +0600

    Create 28.array print.java

---
+++ basic code/28.array print.java
@@ -0,0 +1,20 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int[] number;
+        number=new int[5];
+        number[0]=1;
+        number[1]=10;
+        number[2]=111;
+        number[3]=101;
+        number[4]=100;
+        int len;
+        len=number.length;
+        System.out.println("size of array : "+len);
+        System.out.println(number[0]);
+        System.out.println(number[1]);
+        System.out.println(number[2]);
+        System.out.println(number[3]);
+        System.out.println(number[4]);
+    }
+   
+}
