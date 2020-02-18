7cfd8e9680a39e236cae4aa3229f9f26d3d64cc5
bayazidhasan969 <60389731+bayazidhasan969@users.noreply.github.com>
2020-02-19 00:39:21 +0600

    Create 1004-Simple Product.java

---
+++ URI judge/1004-Simple Product.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int a,b,sum;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        sum=a*b;
+        System.out.println("PROD = "+sum);
+        
+    }
+   
+}
