c83c3571840f760987a04df6cf491abe4215b025
bayazidhasan969 <60389731+bayazidhasan969@users.noreply.github.com>
2020-02-23 12:49:26 +0600

    Create 50.method return.java

---
+++ basic code/50.method return.java
@@ -0,0 +1,13 @@
+
+public class Vreturn {
+    int squre(int x)
+    {
+    return x*x;
+    }
+    public static void main(String[] args) {
+      Vreturn s1=new Vreturn();
+      int sq=s1.squre(3);
+     System.out.println("squre is : "+sq);
+    }
+   
+}
